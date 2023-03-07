f937d9ea20bfab11219d2587f964596adfdf666d
David M <dminerur@outlook.com>
2023-03-06 19:36:59 -0500

    initial starting point

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+    
+      <h1>My Quote Machine</h1>
+      <div className="card">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App

df0b1f6af8b50e101a03b759ca87c661fd5659f0
David M <dminerur@outlook.com>
2023-03-06 21:36:17 -0500

    functionally sound, passes all required unit tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-
+import { useState } from 'react'
 import './App.css'
+import { quotes as myQuotes } from './data/quotes'
 
 function App() {
+  const [index, setIndex] = useState(1)
+  const [quotes, setQuotes] = useState(myQuotes)
+
+const handleClick = () => {
+  const random = Math.floor(Math.random() * quotes.length)
+  setIndex(random)
+}
 
   return (
     <div className="App">
-    
       <h1>My Quote Machine</h1>
-      <div className="card">
-        
+      <div id="quote-box" className="card">
+        <div>
+          <p id="text">{quotes[index].quote}</p>
+          <span id="author">- {quotes[index].author}</span>
+        </div>
+        <div className="card-footer">
+          <button onClick={handleClick}id="new-quote">Get New Quote</button>
+          <a 
+          href={`https://twitter.com/intent/tweet?text=${quotes[index].quote}-${quotes[index].author}`}
+          id="tweet-quote" 
+          target="_blank">Tweet this quote!</a>
+        </div>
       </div>
     </div>
   )
---
+++ src/data/quotes.js
@@ -0,0 +1,7 @@
+export const quotes = [
+
+    {quote: 'Hello World', author: 'Voltaire'},
+    {quote: 'I think, therefore, I am', author: 'Einstein'},
+    {quote: 'I think, therefore, I am', author: 'Descartes'},
+    {quote: 'I think, therefore, I am', author: 'Joan of Arc'}
+]
